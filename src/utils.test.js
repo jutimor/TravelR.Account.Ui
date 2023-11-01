c2959462d9136246b3feac7c1afa329b4accf231
jutimor <julien@thorez.net>
2023-11-01 19:33:44 +0100

    Failed Test

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,6 +2,6 @@ import {currencyFormat} from './utils';
 
 it('should display currency', () => {
     expect(currencyFormat(1)).toBe('1.00 €')
-    expect(currencyFormat(153.5)).toBe('153.50 €')
+    expect(currencyFormat(153.5)).toBe('153.55 €')
     expect(currencyFormat(2153.5)).toBe('2 153.50 €')
 })
